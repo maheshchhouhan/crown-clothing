07976e6f6fea4d682a42d19654d8f5d03493b056
Mahesh Singh Chouhan <mahesh@futureshirts.com>
2019-10-19 03:10:34 +0530

    updating our app to redirect to home if user is signed in

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
@@ -16,7 +17,7 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className="option" to="/contact">
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
       {currentUser ? (
@@ -24,12 +25,16 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="signin">
-          Sign In
+        <Link className="option" to="/signin">
+          SIGN IN
         </Link>
       )}
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
